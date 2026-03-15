fcbcdf7576bb3eb67011f2a978c3ea3451003140
Kuprina Svetlana <kupsveta@rambler.ru>
2026-03-15 21:25:46 +0500

    edit blogs п.2

--- blogs/views.py
+++ blogs/views.py
@@ -1,8 +1,33 @@
-from django.views.generic import ListView, DetailView, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from blogs.models import BlogModel
 
 class BlogListView(ListView):
     model = BlogModel
-    template_name = 'blog.html'
+    template_name = 'blogs/blogs.html'
     context_object_name = 'blogs'
+
+class BlogDetailView(DetailView):
+    model = BlogModel
+    template_name = 'blogs/blog.html'
+    context_object_name = 'blog'
+
+class BlogCreateView(CreateView):
+    model = BlogModel
+    template_name = 'blogs/blog_create.html'
+    fields = ('name', 'content', 'preview', 'publication_attribute', 'number_of_views')
+    success_url = reverse_lazy('blogs:blog-list')
+
+
+class BlogUpdateView(UpdateView):
+    model = BlogModel
+    template_name = 'blogs/blog_create.html'
+    fields = ('name', 'content', 'preview', 'publication_attribute', 'number_of_views')
+    success_url = reverse_lazy('blogs:blog-list')
+
+class BlogDeleteView(DeleteView):
+    model = BlogModel
+    template_name = 'blogs/blog_delete.html'
+    success_url = reverse_lazy('blogs:blog-list')
+
