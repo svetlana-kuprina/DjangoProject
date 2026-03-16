ab67eb006aee007bee678caa02cf8730f5fe83e1
Kuprina Svetlana <kupsveta@rambler.ru>
2026-03-16 20:09:28 +0500

    edit blogs п.3

--- blogs/urls.py
+++ blogs/urls.py
@@ -7,9 +7,9 @@ app_name = BlogsConfig.name
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="blog-list"),
-    path("blog/<int:pk>", BlogDetailView.as_view(), name="blog"),
-    path("blog/create", BlogCreateView.as_view(), name="blog_create"),
-    path("blog/<int:pk>/update", BlogUpdateView.as_view(), name="blog-update"),
-    path("blog/<int:pk>/delete", BlogDeleteView.as_view(), name="blog-delete"),
+    path("blog/<int:pk>/", BlogDetailView.as_view(), name="blog"),
+    path("blog/create/", BlogCreateView.as_view(), name="blog_create"),
+    path("blog/<int:pk>/update/", BlogUpdateView.as_view(), name="blog-update"),
+    path("blog/<int:pk>/delete/", BlogDeleteView.as_view(), name="blog-delete"),
 
 ]
