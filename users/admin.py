7bf1f9d873e5ab3a049e00b8f79c3dab645e0a30
Kuprina Svetlana <kupsveta@rambler.ru>
2026-03-30 19:57:58 +0500

    ДЗ 27-2,3

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "email")
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
+from users.views import RegisterView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
